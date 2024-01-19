3afefa1d80d085473b5242b7f5e9cd7a7059060f
wahyu <wahyu.hdy@gmail.com>
2024-01-19 09:28:40 +0700

    hide https on local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (config('app.env') === 'production') {
+        /*if (config('app.env') === 'production') {
           URL::forceScheme('https');
-        } 
+        }*/
     }
 
     /**
